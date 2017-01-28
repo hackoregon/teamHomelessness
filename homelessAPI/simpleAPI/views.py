cff86987ccdd9df11c173f6f165c544fde8c80a4
Zachary Kent <zak.kent@gmail.com>
2017-01-28 13:47:18 -0800

    changes to imports for python 3

--- homelessAPI/simpleAPI/views.py
+++ homelessAPI/simpleAPI/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from serializers import EthnicitySerializer
-from models import Ethnicity
+from .serializers import EthnicitySerializer
+from .models import Ethnicity
 
 
 class ListEthnicity(generics.ListAPIView):
