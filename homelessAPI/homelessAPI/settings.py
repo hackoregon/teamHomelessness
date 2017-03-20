64024eef9c3676a2dcccbbee28bacdc2760c2767
Zachary Kent <zak.kent@gmail.com>
2017-03-20 10:42:13 -0700

    switched debug flag

--- homelessAPI/homelessAPI/settings.py
+++ homelessAPI/homelessAPI/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = project_config.DJANGO_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
