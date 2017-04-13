92930db0ec179351c093f8fcafc229160d72ce18
mikethecanuck <mikethecanuck@gmail.com>
2017-04-13 08:05:27 -0700

    ensuring the project_config.py gets downloaded inside the launched container

--- homelessAPI/bin/docker-entrypoint.sh
+++ homelessAPI/bin/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH=$PATH:~/.local/bin
-./homelessAPI/bin/getconfig.sh
+./bin/getconfig.sh
 #python manage.py migrate --noinput
 #python manage.py collectstatic --noinput
 gunicorn homelessAPI.wsgi:application -b :8000 --worker-class 'gevent' --workers 3
