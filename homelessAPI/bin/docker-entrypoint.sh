3bc66464d088c32a8e70ac4fe68b633ec3d6d945
mikethecanuck <mikethecanuck@gmail.com>
2017-04-12 22:18:01 -0700

    explicitly specifying the damned path in yet another place

--- homelessAPI/bin/docker-entrypoint.sh
+++ homelessAPI/bin/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH=$PATH:~/.local/bin
-./$PROJ_SETTINGS_DIR/bin/getconfig.sh
+./homelessAPI/bin/getconfig.sh
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn homelessAPI.wsgi:application -b :8000 --worker-class 'gevent' --workers 3
