dd5d3bf3efd0beea43dcc927ac537d9efd767b5d
mikethecanuck <mikethecanuck@gmail.com>
2017-04-15 14:03:44 -0700

    this time we push the built image to ECS

--- homelessAPI/bin/docker-entrypoint.sh
+++ homelessAPI/bin/docker-entrypoint.sh
@@ -3,4 +3,4 @@ export PATH=$PATH:~/.local/bin
 #./bin/getconfig.sh
 #python manage.py migrate --noinput
 #python manage.py collectstatic --noinput
-gunicorn homelessAPI.wsgi:application -b :8000 --worker-class 'gevent' --workers 3
+gunicorn homelessAPI.wsgi:application -b :8000 --worker-class 'gevent' --workers 1
